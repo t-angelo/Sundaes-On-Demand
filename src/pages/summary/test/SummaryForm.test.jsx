1ec45a64b4cd86fbb7c7a911dc34ebb429811f2b
625610 <tsaganeas_angelo@bah.com>
2022-09-08 16:24:38 -0400

    Test and code summary code

---
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -0,0 +1,34 @@
+import { render, fireEvent, screen } from "@testing-library/react";
+import SummaryForm from "../SummaryForm";
+
+test("Initial conditions", () => {
+  render(<SummaryForm />);
+
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+
+  expect(checkbox).not.toBeChecked();
+
+  const confirmButton = screen.getByRole("button", {
+    name: /confirm order/i,
+  });
+  expect(confirmButton).toBeDisabled();
+});
+
+test("Checkbox enables button on a first click and disables on second click", () => {
+  render(<SummaryForm />);
+
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  const confirmButton = screen.getByRole("button", {
+    name: /confirm order/i,
+  });
+
+  fireEvent.click(checkbox);
+  expect(confirmButton).toBeEnabled();
+
+  fireEvent.click(checkbox);
+  expect(confirmButton).toBeDisabled();
+});
