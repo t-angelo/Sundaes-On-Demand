233d4e4adffe4ba8263468fec54957c589fac755
625610 <tsaganeas_angelo@bah.com>
2022-09-19 15:56:30 -0400

    added topping options, updated tests and handlers

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -12,3 +12,15 @@ test('displays image for each scoop option from server', async () => {
     const altText = scoopImages.map(element => element.alt)
     expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop'])
 })
+
+test('displays image for each toppings option from server', async () => {
+    render(<Options optionType="toppings" />)
+
+    // find images
+    const toppingImages = await screen.findAllByRole('img', { name: /topping$/i })
+    expect(toppingImages).toHaveLength(3)
+
+    // confirm alt text of images
+    const imageTitles = toppingImages.map(img => img.alt)
+    expect(imageTitles).toEqual(['Cherries topping', 'M&Ms topping', 'Hot fudge topping'])
+})
